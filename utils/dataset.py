4a9210520883db4e2ba96cc1b85748c3570f2267
Kevin Sasso <kevin@wndyr.com>
2024-05-31 18:11:49 -0700

    Update dataset.py

--- utils/dataset.py
+++ utils/dataset.py
@@ -4,7 +4,7 @@ import pandas as pd
 # only care about images and audio for now, potentially subset based on input type (audio, video, image, etc)
 def is_valid_type(file, type):
     valid = False
-    if file.endswith(".jpg") or file.endswith("jpeg") or file.endswith(".png"):
+    if file.endswith(".jpg") or file.endswith(".jpeg") or file.endswith(".png"):
         if type == "image":
             valid = True
         else:
